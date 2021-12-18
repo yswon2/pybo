ce44659dc5084997332252e9c120e1f301c12ef3
양성원 <yswon2@naver.com>
2021-12-18 17:00:09 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/views/base_views.py
@@ -0,0 +1,43 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+
+from ..models import Question
+
+
+def index(request):
+    '''
+     pybo 목록 출력
+    '''
+
+    #입력 파라미터
+    page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
+
+    question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
+
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list':page_obj, 'page': page, 'kw': kw}
+    #context = {'question_list':question_list}
+
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    내용출력
+    """
+    #question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+
+    return render(request, 'pybo/question_detail.html', context)
+
