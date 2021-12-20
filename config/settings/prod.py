e7c17d2d3e79d40ee0ebebd7425068e463753aa9
양성원 <yswon2@naver.com>
2021-12-20 21:42:58 +0900

    PROD DATABASE 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['13.124.5.37']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '05tkfkdgo!',
+        'HOST': 'ls-c152d9157d388ff408176e07d31a0d9370be7bb6.cmp3sco2upat.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
