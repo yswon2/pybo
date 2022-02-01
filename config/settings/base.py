94d4a3643f4f45e253d9b8e366c1a38ed97bc0d3
양성원 <yswon2@naver.com>
2022-02-02 07:58:31 +0900

    테스트

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os, json
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -19,8 +21,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7001l6%ymwzx^&)jj^t*vw$w^=%4wabhx9v(w%_-f-=c@4!4s)'
+#SECRET_KEY = '7001l6%ymwzx^&)jj^t*vw$w^=%4wabhx9v(w%_-f-=c@4!4s)'
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
